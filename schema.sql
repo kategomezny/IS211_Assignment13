dd7bd5c92c8d21797460de54f042c1deebac21f5
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-11-23 16:47:11 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,34 +1,17 @@
 CREATE TABLE IF NOT EXISTS Student (
-
     student_id  INTEGER PRIMARY KEY AUTOINCREMENT,
-
     first_name TEXT,
-
     last_name TEXT
-
 );
-
 CREATE TABLE IF NOT EXISTS Quiz (
-
     Quiz_id INTEGER PRIMARY KEY AUTOINCREMENT,
-
     Subject TEXT,
-
     Number_of_questions INTEGER,
-
     Date TEXT
-
 );
-
 CREATE TABLE IF NOT EXISTS Results (
-
     Record_id INTEGER PRIMARY KEY AUTOINCREMENT,
-
     Student_id INTEGER,
-
     Quiz_id INTEGER,
-
     Score INTEGER
-
 );
-
